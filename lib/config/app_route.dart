fc69eed2664b3229c02ab256a2ede63241827308
AlescoZai <9b.dhimasulung@gmail.com>
2024-06-06 23:13:01 +0700

    feat: route page app and build sign in

---
+++ lib/config/app_route.dart
@@ -0,0 +1,10 @@
+class AppRoute {
+  static const String intro = '/intro';
+  static const String home = '/home';
+  static const String signin = '/signIn';
+  static const String detail = '/detail';
+  static const String checkout = '/checkout';
+  static const String checkoutSuccess = '/checkout_success';
+  static const String detailBooking = '/detail_booking';
+}
+
